c1d8c10e0b85b7b620c8455cdf3544ec630f8bbf
arianepaula <arianepaula04@gmail.com>
2016-09-05 17:17:25 -0300

    Inserindo exemplos com Calendar

--- src/CalendarExample.java
+++ src/CalendarExample.java
@@ -7,11 +7,40 @@ public class CalendarExample {
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        Calendar now = Calendar.getInstance();
-        System.out.println(sdf.format(now.getTime()));
+//        Retorna a data atual c/ Calendar
 
-        now.add(Calendar.MONTH, -1);
-        System.out.println(sdf.format(now.getTime()));
+        Calendar now = Calendar.getInstance();
+        System.out.println("Data atual: " + sdf.format(now.getTime()));
+
+//        Compara duas datas e retorna se são iguais c/ Calendar
+
+        Calendar firstDate = Calendar.getInstance();
+        Calendar secondDate = Calendar.getInstance();
+
+        System.out.println("Data a ser comparada: " + sdf.format(firstDate.getTime()));
+        System.out.println("Retorno compareTo first e secondDate: " + firstDate.compareTo(secondDate));
+
+//        Setando um valor para a data c/ Calendar
+
+        Calendar date = Calendar.getInstance();
+        date.set(Calendar.DAY_OF_MONTH, 30);
+        date.set(Calendar.MONTH, 3);
+        date.set(Calendar.YEAR, 2015);
+        System.out.println("Setando um valor para a data: " + sdf.format(date.getTime()));
+
+//        Adicionando dias, meses e anos c/ Calendar
+
+        Calendar dateAddDays = Calendar.getInstance();
+        dateAddDays.add(Calendar.DAY_OF_MONTH, 10);
+        System.out.println("Adicionando dias: " + sdf.format(dateAddDays.getTime()));
+
+        Calendar dateAddMonths = Calendar.getInstance();
+        dateAddMonths.add(Calendar.MONTH, 1);
+        System.out.println("Adicionando meses: " + sdf.format(dateAddMonths.getTime()));
+
+        Calendar dateAddYears = Calendar.getInstance();
+        dateAddYears.add(Calendar.YEAR, 5);
+        System.out.println("Adicionando anos: " + sdf.format(dateAddYears.getTime()));
 
         // Data de agora
         // Setta valor na data
--- src/DateExample.java
+++ src/DateExample.java
@@ -1,2 +1,13 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateExample {
+    public static void main(String[] args) {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        date.setMonth(2);
+
+        System.out.println(sdf.format(date));
+    }
 }
